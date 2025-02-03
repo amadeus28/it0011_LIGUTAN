c5cca7c6645ff1212ad1c3b6aee198bdbb2b8a2e
amadeus28 <ligutanaldrinlorenz@gmail.com>
2025-02-03 10:50:21 +0800

    new

--- TFA2/3.3.py
+++ TFA2/3.3.py
@@ -3,12 +3,12 @@ fname = input("Enter first name: ")
 age = (input("Enter age: "))
 contact = input("Enter contact number: ")
 course = input("Enter course: ")
-lines = ("Last Name: "+ lname+
+
+f=open("C:\studentinfo.txt", "a")
+f.write("Last Name: "+ lname+
          "First Name: "+ fname+
          "Age: "+ age+
          "Contact Number: "+ contact+
          "Course: "+ course)
-f=open("studentinfo.txt", "a")
-f.write(lines)
 f.close()
 print("Student information has been saved to \'studentinfo.txt\'")
