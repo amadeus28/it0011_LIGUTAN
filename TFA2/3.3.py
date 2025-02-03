81fd3fdfadf55b25fb17312b58ea0d7474ef7976
amadeus28 <ligutanaldrinlorenz@gmail.com>
2025-02-03 10:53:34 +0800

    newwew

--- TFA2/3.3.py
+++ TFA2/3.3.py
@@ -4,11 +4,12 @@ age = (input("Enter age: "))
 contact = input("Enter contact number: ")
 course = input("Enter course: ")
 
-f=open("C:\studentinfo.txt", "a")
-f.write("Last Name: "+ lname+
-         "First Name: "+ fname+
-         "Age: "+ age+
-         "Contact Number: "+ contact+
-         "Course: "+ course)
+student_info = ("Last Name: " {lname}
+         "First Name: " {fname}
+         "Age: " {age}
+         "Contact Number: "{contact}
+         "Course: "{course})
+f=open("C:\Users\202311140\Documents\GitHub\it0011_LIGUTAN\TFA2", a")
+f.write()
 f.close()
 print("Student information has been saved to \'studentinfo.txt\'")
