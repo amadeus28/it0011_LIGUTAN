dcad4de2addbcbae17d086fda483476f0b6683ab
amadeus28 <ligutanaldrinlorenz@gmail.com>
2025-02-13 19:57:45 +0800

    last

--- Technical Midterm Exam/technical_midterm_1.py
+++ Technical Midterm Exam/technical_midterm_1.py
@@ -1,11 +1,13 @@
-file = open('numbers.txt', 'r')  
+file = open('Technical Midterm Exam\\numbers.txt', 'r')
 lines = file.readlines()
 file.close()
 
-for i, line in enumerate(lines, start=1):
+line_number = 1  
+
+for line in lines:
     numbers = line.strip().split(',')
     total_sum = sum(int(num) for num in numbers)
-    
     palindrome_status = "Palindrome" if str(total_sum) == str(total_sum)[::-1] else "Not a palindrome"
-
-    print(f"Line {i}: {line.strip()} (sum {total_sum}) - {palindrome_status}")
+    
+    print(f"Line {line_number}: {line.strip()} ({total_sum}) - {palindrome_status}")
+    line_number += 1  
--- Technical Midterm Exam/technical_midterm_2.py
+++ Technical Midterm Exam/technical_midterm_2.py
@@ -1,9 +1,7 @@
 from datetime import datetime
 
 date_input = input("Enter the date (mm/dd/yyyy): ")
-
 date_obj = datetime.strptime(date_input, "%m/%d/%Y")
-
 formatted_date = date_obj.strftime("%B %d, %Y")
 
 print("Date Output:", formatted_date)
